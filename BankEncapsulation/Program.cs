02e1d3272ad68328fda3f1c019aafe9fb6a5f220
mvdoyle <mdoyle@truecoders.io>
2020-04-13 13:49:23 -0500

    finsished

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var michaelsAccount = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            michaelsAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you!  Your balance is now {michaelsAccount.GetBalance()}");
+        }
+    }
+}
